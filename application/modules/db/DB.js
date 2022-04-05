a1c167261af92de24272abfa662e722a3068ce3e
PigalovMaxim <101743221+PigalovMaxim@users.noreply.github.com>
2022-04-05 11:01:00 +0400

    ORM ADDED

--- application/modules/db/DB.js
+++ application/modules/db/DB.js
@@ -1,22 +1,20 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
-const { Socket } = require('socket.io');
-const md5 = require('md5');
+const ORM = require('./ORM');
 
 class DB {
     constructor({ HOST, NAME, USER, PASS }, mediator){
         this.mediator = mediator;
-        //this.db.get();
-        //this.db.run();
-        //this.db.all();
         
         this.db = new sqlite3.Database(path.join(__dirname, NAME));
-        this.mediator.subscribe(this.mediator.EVENTS.USER_REGISTRATION, (data) => this.registration(data));
+        this.orm = new ORM(this.db);
+        setTimeout(() => this.insert({nick: 'OOOOOO', hash: 'asdasd', socket_id: 'asdasdawd'}), 2500);
+        /* this.mediator.subscribe(this.mediator.EVENTS.USER_REGISTRATION, (data) => this.registration(data));
         this.mediator.set(this.mediator.TRIGGERS.GET_USER_BY_NICK, nick => this._getUserByNick(nick));
         this.mediator.set(this.mediator.TRIGGERS.GET_USER_BY_ID, id => this._getUserById(id));
-        this.mediator.set(this.mediator.TRIGGERS.LOGIN, data => this.login(data));
+        this.mediator.set(this.mediator.TRIGGERS.LOGIN, data => this.login(data)); */
     }
-    
+
     destructor() {
         if(this.db){
           this.db.close();
@@ -38,30 +36,28 @@ class DB {
         return this._getUserByNick(nick);
     }
 
-    _getUserById(id) {
-        return new Promise(resolve => {
-            this.db.serialize(() => {
-                const query = "SELECT * FROM users WHERE socket_id=?";
-                this.db.get(
-                    query,
-                    [id],
-                    (err, row) => resolve(err ? null : row)
-                );
-            });
-        });
+    _getUserById(socket_id) {
+        return this.orm.get('users', {socket_id});
     }
 
-    _getUserByNick(nick){
-        return new Promise(resolve => {
-            this.db.serialize(() => {
-                const query = "SELECT * FROM users WHERE nick=?";
-                this.db.get(
-                    query,
-                    [nick],
-                    (err, row) => resolve(err ? null : row)
-                );
-            });
-        });
+    async _getUserByNick(nick){
+        let a = await this.orm.all('users');
+        console.log(a);
+    }
+
+    async deleteUser(params){
+        let a = await this.orm.delete('users', params);
+        console.log(a);
+    }
+
+    async changeParam(setParams, whereParams){
+        let a = await this.orm.update('users', whereParams, setParams);
+        console.log(a);
+    }
+
+    async insert(params){
+        let a = await this.orm.insert('users', params);
+        console.log(a);
     }
 
     registration({nick, hash, id}){
@@ -75,6 +71,3 @@ class DB {
 }
 
 module.exports = DB;
-
-/*НЕ РАБОТАЕТ КНОПКА ЛОГИНА
-*/
---
+++ application/modules/db/ORM.js
@@ -0,0 +1,100 @@
+class ORM {
+    constructor(db){
+        this.db = db;
+    }
+
+    //Получение одной записи
+    get(tables, params = {}, fields = '*', operand = 'AND') {
+        return new Promise(resolve => {
+            this.db.serialize(() => {
+                const str = Object.keys(params).map(key => `${key}=?`).join(` ${operand} `);
+                const arr = Object.values(params);
+                const query = `
+                    SELECT ${fields} 
+                    FROM ${tables instanceof Array ? tables.join(', ') : tables} 
+                    ${str.length === 0 ? '' : `WHERE ${str}`}`;
+                this.db.get(
+                    query,
+                    arr,
+                    (err, row) => resolve(err ? null : row)
+                );
+            });
+        });
+    }
+
+    all(tables, params = {}, fields = '*', operand = 'AND') {
+        return new Promise(resolve => {
+            this.db.serialize(() => {
+                const str = Object.keys(params).map(key => `${key}=?`).join(` ${operand} `);
+                const arr = Object.values(params);
+                const query = `
+                    SELECT ${fields} 
+                    FROM ${tables instanceof Array ? tables.join(', ') : tables} 
+                    ${str.length === 0 ? '' : `WHERE ${str}`}`;
+                this.db.all(
+                    query,
+                    arr,
+                    (err, rows) => resolve(err ? [] : rows)
+                );
+            });
+        });
+    }
+
+    delete(tables, params = {}, operand = 'AND') {
+        return new Promise(resolve => {
+            this.db.serialize(() => {
+                const str = Object.keys(params).map(key => `${key}=?`).join(` ${operand} `);
+                const arr = Object.values(params);
+                const query = `
+                    DELETE FROM ${tables instanceof Array ? tables.join(', ') : tables} 
+                    ${str.length === 0 ? '' : `WHERE ${str}`}`;
+                this.db.run(
+                    query,
+                    arr,
+                    (err) => resolve(!err)
+                );
+            });
+        });
+    }
+
+    update(tables, whereParams = {}, setParams = {}, operand = 'AND') {
+        return new Promise(resolve => {
+            this.db.serialize(() => {
+                const whereStr = Object.keys(whereParams).map(key => `${key}=?`).join(` ${operand} `);
+                const whereArr = Object.values(whereParams);
+                const setStr = Object.keys(setParams).map(key => `${key}=?`).join(`, `);
+                const setArr = Object.values(setParams);
+                const query = `
+                    UPDATE ${tables instanceof Array ? tables.join(', ') : tables} 
+                    SET ${setStr}
+                    ${whereStr.length === 0 ? '' : `WHERE ${whereStr}`}`;
+                this.db.run(
+                    query,
+                    [setArr ,whereArr],
+                    (err) => resolve(!err)
+                );
+            });
+        });
+    }
+
+    insert(table, params = {}) {
+        return new Promise(resolve => {
+            this.db.serialize(() => {
+                const str = Object.keys(params).join(`, `);
+                const arr = Object.values(params);
+                const query = `
+                    INSERT INTO ${table} (${str})
+                    ${arr.length === 0 ? '' : `VALUES (${arr.join(', ')})`}`;
+
+                    console.log(query);
+                this.db.run(
+                    query,
+                    arr,
+                    (err) => resolve(!err)
+                );
+            });
+        });
+    }
+}
+
+module.exports = ORM;
